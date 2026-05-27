271d48a8130b55cc652e5d4a53b2e18c28b26324
jmangion4 <josephmangion48@gmail.com>
2026-05-27 10:00:08 +0200

    Delete stackoverflow.c

--- stackoverflow.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <limits.h>
-
-int main() {
-    int x = INT_MAX; // The largest possible value for an int
-    printf("Before: %d\n", x);
-    printf("After:  %d\n", x + 1); // Overflows to the minimum possible value by adding 1 to the maximum value
-    return 0;
-}
